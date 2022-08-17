871fbf9d3d1aaa793942f369830283d8b7ea729c
Suvabose05 <37765538+Suvabose05@users.noreply.github.com>
2022-08-17 19:21:01 +0530

    Add files via upload

---
+++ authorization-service/src/main/java/com/cts/authorization/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.cts.authorization.exception;
+
+
+public class InvalidCredentialsException extends RuntimeException {
+
+	private static final long serialVersionUID = -4836014323607899641L;
+
+	public InvalidCredentialsException(String message) {
+		super(message);
+	}
+}
